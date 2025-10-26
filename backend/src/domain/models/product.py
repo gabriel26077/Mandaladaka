8eef708eb50889f8bf41216bc991eac1a6a63df1
gabriel26077 <gabrielsebastiao321@gmail.com>
2025-10-26 12:52:06 -0300

    feat(back): cria modelo usuário como modelo de entidade de domínio

---
+++ backend/src/domain/models/product.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Product:
+    """
+    Representa um Produto no nosso domínio de negócio.
+    É um objeto de dados puro (DTO) usado internamente.
+    """
+    id: int
+    name: str
+    price: float
+    availability: bool
+    category: str
+    imageUrl: str
+
+
